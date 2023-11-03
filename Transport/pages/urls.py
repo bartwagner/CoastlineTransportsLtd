9527d78bbeb42b71e183f5975aeeace2b6f60e9e
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-11-03 16:48:40 -0700

    schedule created

--- Transport/pages/urls.py
+++ Transport/pages/urls.py
@@ -4,8 +4,21 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     path('faq', views.faq, name='faq'),
     path('safety', views.safety, name='safety'),
     path('shipping', views.shipping, name='shipping'),
-    path('charterBus', views.charterBus, name='charterBus')
+    path('charterBus', views.charterBus, name='charterBus'),
+    path('tickets', views.tickets, name='tickets'),
+    path('schedules', views.schedules, name='schedules'),
+    path('bookTrip', views.bookTrip, name='bookTrip'),
+    path('contactUs', views.contactUs, name='contactUs'),
+
+
+
+    path('loyaltyRegister', views.loyaltyRegister, name='loyaltyRegister'),
+    path('privacyPolicy', views.privacyPolicy, name='privacyPolicy'),
+    path('termsConditions', views.termsConditions, name='termsConditions'),
+    path('cancelChangesReserve', views.cancelChangesReserve, name='cancelChangesReserve')
 ]
--- Transport/pages/views.py
+++ Transport/pages/views.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'pages/index.html')
 
+def register(request):
+    return render(request, 'pages/register.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
 def faq(request):
     return render(request, 'pages/faq.html')
 
@@ -16,3 +22,32 @@ def shipping(request):
 
 def charterBus(request):
     return render(request, 'pages/charterBus.html')
+
+def tickets(request):
+    return render(request, 'pages/tickets.html')
+
+def schedules(request):
+    return render(request, 'pages/schedules.html')
+
+def bookTrip(request):
+    return render(request, 'pages/bookTrip.html')
+
+def contactUs(request):
+    return render(request, 'pages/contactUs.html')
+
+
+
+
+
+
+def loyaltyRegister(request):
+    return render(request, 'pages/loyaltyRegister.html')
+
+def privacyPolicy(request):
+    return render(request, 'pages/privacyPolicy.html')
+
+def termsConditions(request):
+    return render(request, 'pages/termsConditions.html')
+
+def cancelChangesReserve(request):
+    return render(request, 'pages/cancelChangesReserve.html')
