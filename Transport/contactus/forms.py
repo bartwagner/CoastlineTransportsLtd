c1cfb7bbbb8fe7400119d686a27829e13871494e
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-11-14 14:40:31 -0800

    new jquery and form

--- Transport/contactus/forms.py
+++ Transport/contactus/forms.py
@@ -1,14 +1,26 @@
 from django import forms
 
 class contactUsForm(forms.Form):
-    name = forms.CharField(max_length=50, 
-                           widget=forms.TextInput(attrs={"class": "form-control"}), 
+    name = forms.CharField(
+                           max_length=50,
                            label="Name:"
                           )
-    email = forms.EmailField(widget=forms.TextInput(attrs={"type": "email"}), 
+    email = forms.EmailField(
+                             max_length=90,
+                             widget=forms.TextInput(attrs={"type": "email"}), 
                              label="E-mail:"
                             )
-    phone = forms.CharField(max_length=13, 
-                            widget=forms.NumberInput(attrs={"type": "number"}), 
+    phone = forms.CharField(
+                            widget=forms.TextInput(attrs={"type": "tel"}),
                             label="Phone:"
                            )
+    ticket = forms.CharField(
+                             max_length=25,
+                             required=False, 
+                             label="Ticket:"
+                            )
+    message = forms.CharField(
+                              max_length=1000,
+                              widget=forms.Textarea,
+                              label="Speak your mind, please:"
+                             )
