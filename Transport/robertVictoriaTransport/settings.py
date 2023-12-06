a946f2bdb6e52047e4f4af7b8853f31d328df040
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-12-05 19:06:09 -0800

    media folder

--- Transport/robertVictoriaTransport/settings.py
+++ Transport/robertVictoriaTransport/settings.py
@@ -133,6 +133,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'robertVictoriaTransport/static')
 ]
 
+#Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
