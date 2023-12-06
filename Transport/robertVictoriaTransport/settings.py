77a200299c9d6e53279878824ecb4770cacd6029
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-12-06 13:19:20 -0800

    customize admin area

--- Transport/robertVictoriaTransport/settings.py
+++ Transport/robertVictoriaTransport/settings.py
@@ -133,10 +133,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'robertVictoriaTransport/static')
 ]
 
-#Media Folder Settings
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 # Messages
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
