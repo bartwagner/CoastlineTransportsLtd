722f511cbf03e9e142bfd8ad38d0c3d9563a88de
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-12-12 17:10:20 -0800

    calendar

--- Transport/booktrip/views.py
+++ Transport/booktrip/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
 from datetime import datetime
-#from .models import Travel
 
-#from .models import DateTimeTravel
+from .models import TravelBoard
+from .models import TravelDestination
+from .models import Travel
+from .models import LocalBoard
+from .models import LocalDestination
+from .models import DateTimeTravel
 
 def index(request):
- #   travel = Travel.objects.first()
-
-  #  print(travel)
+    yearDate = datetime.now().year
+    monthDate = datetime.now().month
+    dayDate = datetime.now().day
+    
+    cal = HTMLCalendar().formatmonth(yearDate, monthDate, dayDate)
 
     #dateTimeTravel = DateTimeTravel.objects.filter(date__gte=datetime.now().date()).order_by('-date')
+    #print(dateTimeTravel.values_list('date', flat=True))
 
     #print(datetime.now().date())
     #print(DateTimeTravel.objects.first().date)
@@ -20,5 +29,5 @@ def index(request):
     #}
 
     #return render(request, 'booktrip/booktrip.html', context)
-    return render(request, 'booktrip/booktrip.html')
+    return render(request, 'booktrip/booktrip.html', {'cal' : cal})
 
