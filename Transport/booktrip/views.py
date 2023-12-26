575000dd83583da278faaf847c96cb9f3e12602a
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-12-25 16:36:21 -0800

    returning the select option

--- Transport/booktrip/views.py
+++ Transport/booktrip/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 from datetime import datetime
 from django.http import JsonResponse
 
 from .models import DateTimeTravel
+
 #from .models import TravelBoard
 #from .models import TravelDestination
 #from .models import Travel
@@ -21,9 +24,28 @@ def index(request):
 
 def handle_selected_date(request):
     if request.method == 'GET' and 'selectedDateText' in request.GET:
-        selected_date = request.GET['selectedDateText']
-        response_data = {'selected_date': selected_date}
+        selected_date_text = request.GET['selectedDateText']
 
-        return JsonResponse(response_data)
+        try:
+            #Try to retrieve the DateTimeTravel object for the selected date.
+            selected_date = datetime.strptime(selected_date_text, '%m/%d/%Y').strftime('%Y-%m-%d')
+            date_time_travel = DateTimeTravel.objects.get(date=selected_date)
+
+            #Now, you can retrieve the TravelBoard object associated with this DateTimeTravel.
+            travel_board = date_time_travel.travel.travelboard
+
+            #Combine the selected date with the current time for comparison.
+            #selected_datetime = datetime.combine(date_time_travel.date, travel_board.time_board)
+            #if selected_datetime < datetime.now(): I will check it after
+
+            response_data = {
+                'board': travel_board.board,
+            }
+
+            return JsonResponse(response_data)
+        
+        except DateTimeTravel.DoesNotExist:
+            return JsonResponse({'error': 'No matching DateTimeTravel found for the selected date'})
+        
     else:
         return JsonResponse({'error': 'Invalid request'})
